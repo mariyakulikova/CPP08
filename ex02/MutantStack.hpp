8d0f27cfcd386e294662bddd69d7a8a48eb1e611
Mariia Kulikova <mkulikov@student.42berlin.de>
2025-05-22 17:43:01 +0200

    feat(ex02) init task

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/22 17:41:21 by mkulikov          #+#    #+#             */
+/*   Updated: 2025/05/22 17:42:14 by mkulikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+class MutantStack
+{
+private:
+
+public:
+	MutantStack();
+	~MutantStack();
+};
+
+#endif
